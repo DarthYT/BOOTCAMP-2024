2f9ff4eac50254b0d4ce68edfc28a67aac3ee2fa
DarthYT <yavuztugrul7@gmail.com>
2024-09-25 21:26:06 +0300

    s

---
+++ ortak/yaz.js
@@ -0,0 +1,12 @@
+document.body.style.background = "black";
+document.body.style.color = "white";
+document.body.style.fontSize = "3em";
+document.body.style.fontFamily = '"Courier New", monospace';
+
+function yaz(html = "") {
+    document.write(html + "<br>");
+}
+
+function ciz(html = "") {
+    document.write(html + "<hr>");
+}
